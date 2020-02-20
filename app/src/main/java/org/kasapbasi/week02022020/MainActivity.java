8db97862452810f1688090384b64ff0ce5bf8f2c
mckasap <mckasap@gmail.com>
2020-02-20 16:04:52 +0300

    Resim okuma

--- app/src/main/java/org/kasapbasi/week02022020/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week02022020/MainActivity.java
@@ -2,13 +2,27 @@ package org.kasapbasi.week02022020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv=(ImageView) findViewById(R.id.imageView);
+
+        try{
+            iv.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM) );
+
+        }catch (Exception ex){
+
+
+        }
+
     }
 }
